e078d9c0602f8f1c6115549e410a8581dae1c67f
Renan Dutra <r.granatodutra@gmail.com>
2024-12-04 11:58:30 -0300

    feat: update logging utility to conditionally output debug messages in development environment

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,24 +1,22 @@
 class Log {
     public static info(message: string) {
         const now = new Date();
-
         console.log(`${now.toLocaleString()} [Info] ${message}`);
     }
 
     public static error(message: string, error?: any) {
         const now = new Date();
-
         console.error(`${now.toLocaleString()} [Error] ${message}`);
-
         if (error) {
             console.error(error);
         }
     }
 
     public static debug(message: string) {
-        const now = new Date();
-
-        console.debug(`${now.toLocaleString()} [Debug] ${message}`);
+        if (process.env.NODE_ENV === 'development') {
+            const now = new Date();
+            console.debug(`${now.toLocaleString()} [Debug] ${message}`);
+        }
     }
 }
 
